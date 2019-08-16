c475252172718d350e0f1eec6cbabf696a8fe7b6
Carlos Torres <carlos.torres@cacira.com>
2019-08-16 12:26:34 -0400

    Improved the blacklist of ads providing domains.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 var enabled = true;
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
-        //console.log("blocking:", details.url);
+        console.log("blocking:", details.url);
 		return {cancel: enabled };
 	},
 	{urls: blocked_domains},
